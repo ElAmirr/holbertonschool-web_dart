7a94169d66a1e216aabe88f139997ea682b88ffb
ElAmirr <othmaniamir41@gmail.com>
2024-12-06 21:41:22 +0100

    push dart codes

---
+++ 0x00-hello-dart/5-assertion.dart
@@ -0,0 +1,5 @@
+void main() {
+  var nb = 79; // Replace this with the actual value you want to test
+  assert(nb >= 80, 'The score must be bigger or equal to 80');
+  print('You Passed');
+}
---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  if (args.isEmpty) {
+    print('Please provide a number as an argument.');
+    return;
+  }
+  
+  var number = int.parse(args[0]);
+  if (number > 0) {
+    print('$number is positive');
+  } else if (number == 0) {
+    print('$number is zero');
+  } else {
+    print('$number is negative');
+  }
+}
